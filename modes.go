a5206322faa872b65f60b902b15a551497a80f9f
StephenLangHall <stephenlh2007@gmail.com>
2024-12-31 13:48:46 -0500

    Backup and restore works propperly

--- modes.go
+++ modes.go
@@ -9,11 +9,14 @@ import (
 
 func SaveBoard(b [h][w]bool) {
 	d1 := []byte{}
+	fmt.Println("Hight -> "+strconv.Itoa(h)+"\nWidth -> "+strconv.Itoa(w)+"\n")
 	d1 = append(d1, byte(h), byte(w))
 	for y, row := range b {
 		for x, cell := range row {
 			if cell {
-				d1 = append(d1, byte(y), byte(x))
+				fmt.Println(strconv.Itoa(x) + " x " + strconv.Itoa(y))
+				d1 = append(d1, byte(y))
+				d1 = append(d1, byte(x))
 			}
 		}
 	}
@@ -21,6 +24,7 @@ func SaveBoard(b [h][w]bool) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("===================")
 }
 
 func OpenBoard() [h][w]bool {
@@ -31,13 +35,17 @@ func OpenBoard() [h][w]bool {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if len(dat) < 3 {
+		return nb
+	}
 	nh := int(dat[1])
 	nw := int(dat[0])
 	if h != nh || w != nw {
 		log.Fatal("Hight "+strconv.Itoa(h)+" -> "+strconv.Itoa(nh)+"\nWidth "+strconv.Itoa(w)+" -> "+strconv.Itoa(nw)+"\n")
 	}
-	data := dat[3:]
+	data := dat[2:]
 	for i := range len(data)/2 {
+		fmt.Println(i)
 		ty := int(data[i*2])
 		tx := int(data[i*2+1])
 		fmt.Println(strconv.Itoa(tx) + " x " + strconv.Itoa(ty))
@@ -46,6 +54,7 @@ func OpenBoard() [h][w]bool {
 		}
 	}
 	fmt.Println("Hight "+strconv.Itoa(h)+" -> "+strconv.Itoa(nh)+"\nWidth "+strconv.Itoa(w)+" -> "+strconv.Itoa(nw))
+	fmt.Println("===================")
 	return nb
 }
 
